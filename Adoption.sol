158fe33055bfe99674ea060103f99a22ed4e3a2e
sifunde <42275429+sifunde@users.noreply.github.com>
2018-08-27 00:40:34 +0200

    Create Adoption.sol

---
+++ Adoption.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.17;
+
+contract Adoption {
+address [16] public adopters;
+function adopt (uint petId) public returns (uint) {
+require (petId .= 0 && petId ,= 15);
+
+adopters[petId] = msg.sender;
+
+retrun petId;
+
+function getAdopters () public view returns (address [16]) {
+return adopters;
+truffle compile
+
+}
+
+
+
+
+
+
